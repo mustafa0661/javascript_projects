f79be8753921ea47bbf5f509b2846a5538172baa
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2023-11-03 19:01:58 +0300

    Karşılaştırma operatörleri eklendi

--- operatörler/script.js
+++ operatörler/script.js
@@ -10,3 +10,18 @@ console.log(x+y);
 console.log(x-y);
 console.log(y/x);
 console.log(x*y);
+
+console.log(x+y+z);
+console.log(x*(y+z));
+
+
+console.log(x);
+
+/*KArşılaştırma operatörlei*/
+
+var sonuc = x < y;
+console.log(sonuc);
+
+console.log(x == "10");
+console.log(x === "10");
+console.log(x!=z);
